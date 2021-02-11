630c368a59d211d1d8bd8e2821fe620bf3eb5315
Charles An <mucollabo@gmail.com>
2021-02-12 03:24:43 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# webscrapWpython2.7
---
+++ wikiSpider/wikiSpider/spiders/articleSpider.py
@@ -0,0 +1,21 @@
+# -✯- coding: utf-8
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy.selector import Selector
+from scrapy import Spider
+from wikiSpider.items import Article
+
+class ArticleSpider(Spider):
+    name="article"
+    allowed_domains=["en.wikipedia.org"]
+    start_urls = ["http://en.wikipedia.org/wiki/Main_Page", "http://en.wikipedia.org/wiki/Python_%28programming_language%29"]
+
+    def parse(self, response):
+        item = Article()
+        title = response.xpath('//h1/text()')[0].extract()
+        print("Title is: "+title)
+        item['title'] = title
+        return item
